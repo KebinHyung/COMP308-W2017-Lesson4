d0bd34841a1f202bbeeb2db736072532fc1cfd9a
Kevin Luu <kevin_luu_naruto@hotmail.com>
2017-02-04 15:13:42 -0500

    changes to index files

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,43 @@
 let express = require('express');
 let router = express.Router();
 
-//Global Route Variables
-  let currentDate = new Date();
+// Global Route Variables
+let currentDate = new Date();
   currentDate = currentDate.toLocaleTimeString();
 
-/* GET home page. */
+/* GET home page. wildcard */
 router.get('/', (req, res, next) => {
-
-  res.render('content/index', { 
-    title: 'Home',
-  });
+  res.render('content/index', {
+    title: 'Home'
+   });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('content/about', { title: 'About' });
+  res.render('content/about', {
+    title: 'About'
+   });
 });
-/* GET projects page. */
+
+/* GET products page. */
 router.get('/projects', (req, res, next) => {
-  res.render('content/projects', { title: 'Projects' });
+  res.render('content/projects', {
+    title: 'Projects'
+   });
 });
-/* GET Services page. */
+
+/* GET services page. */
 router.get('/services', (req, res, next) => {
-  res.render('content/services', { title: 'Services' });
+  res.render('content/services', {
+    title: 'Services'
+   });
 });
-/* GET Contact page. */
+
+/* GET contact page. */
 router.get('/contact', (req, res, next) => {
-  res.render('content/contact', { title: 'Contact' });
+  res.render('content/contact', {
+    title: 'Contact'
+   });
 });
 
 module.exports = router;
